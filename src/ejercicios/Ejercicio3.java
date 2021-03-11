e93a3fbd6101fea3fbbfba67910c3234ff71299c
ShinyPotat <59798595+ShinyPotat@users.noreply.github.com>
2021-03-11 23:17:23 +0100

    ex3 C

--- src/ejercicios/Ejercicio3.java
+++ src/ejercicios/Ejercicio3.java
@@ -1,12 +1,15 @@
 package ejercicios;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
+import java.util.TreeSet;
 
 import org.jgrapht.Graph;
 import org.jgrapht.graph.DefaultEdge;
 import org.jgrapht.graph.SimpleDirectedGraph;
+import org.jgrapht.nio.Attribute;
+import org.jgrapht.nio.DefaultAttribute;
 
 import us.lsi.colors.GraphColors;
 import us.lsi.colors.GraphColors.Color;
@@ -14,8 +17,8 @@ import us.lsi.graphs.Graphs2;
 
 public class Ejercicio3 {
 	
-	public static List<String> apartadoA(Map<String, List<String>> map) {
-		List<String> r = new ArrayList<>();
+	public static Set<String> apartadoA(Map<String, Set<String>> map) {
+		Set<String> r = new HashSet<>();
 		while(r.size() != map.keySet().size()) {
 			for(String key: map.keySet()) {
 				if(!r.contains(key)) {
@@ -32,8 +35,8 @@ public class Ejercicio3 {
 		return r;
 	}
 	
-	public static List<String> apartadoB(Map<String, List<String>> map) {
-		List<String> r = new ArrayList<>();
+	public static Set<String> apartadoB(Map<String, Set<String>> map) {
+		Set<String> r = new HashSet<>();
 		for(String key: map.keySet()) {
 			if(map.get(key).isEmpty()) {
 				r.add(key);
@@ -52,6 +55,50 @@ public class Ejercicio3 {
 				v -> GraphColors.getColorIf(Color.blue, Color.black, r.contains(v)),
 				e -> GraphColors.getColor(Color.black));
 		return r;
+	}
+	
+	public static Set<String> apartadoC(Map<String, Set<String>> map, Set<String> set) {
+		Set<String> r = new TreeSet<>();
+		for(String key: map.keySet()) {
+			if(!set.contains(key)) {
+				if(map.get(key).isEmpty()) {
+					r.add(key);
+				}else {
+					if(set.containsAll(map.get(key))) {
+						r.add(key);
+					}
+				}
+			}
+		}
+		
+		Graph<String, DefaultEdge> graph = new SimpleDirectedGraph<String, DefaultEdge>(DefaultEdge.class);
+		for(String key: map.keySet()) {
+			graph.addVertex(key);
+			for(String s: map.get(key)) {
+				graph.addEdge(s, key);
+			}
+		}
+		
+		Graphs2.toDot(graph, "ficheros/Ej3.dot",
+				v -> v.toString(),
+				e -> "",
+				v -> getColorIf(v, set, r),
+				e -> GraphColors.getColor(Color.black));
+		
+		return r;
+	}
+	
+	private static Map<String,Attribute> getColorIf(String v, Set<String> cursado, Set<String> pendientes) {		
+		Color c = Color.black;
+		if(cursado.contains(v)) {
+			c = Color.blue;
+		}else if(pendientes.contains(v)) {
+			c = Color.orange;
+		}	
+		
+		String cl = c.toString();
+		Map<String,Attribute> m = Map.of("color", DefaultAttribute.createAttribute(cl));		
+		return m;
 	}
 	
 }
--- src/tests/Test3.java
+++ src/tests/Test3.java
@@ -1,8 +1,9 @@
 package tests;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.TreeMap;
 
 import ejercicios.Ejercicio3;
@@ -13,7 +14,7 @@ public class Test3 {
 	public static void execute() {
 		System.out.println("==== EJERCICIO 3 ====");
 		List<String> lines = Files2.linesFromFile("ficheros/PI5Ej3DatosEntrada.txt");
-		Map<String, List<String>> map = new TreeMap<>();
+		Map<String, Set<String>> map = new TreeMap<>();
 		for(String line: lines) {
 			String[] format = line.split(": ");
 			String asignatura = format[0];
@@ -21,23 +22,43 @@ public class Test3 {
 			int beginIndex = s.indexOf("{");
 			int endIndex = s.indexOf("}");
 			String[] array = format[1].substring(beginIndex+1, endIndex).split(",");
-			List<String> ls = new ArrayList<>();
+			Set<String> set = new HashSet<>();
 			if(!array[0].isBlank()) {
-				ls = List.of(array);
+				set = Set.of(array);
 			}
-			map.put(asignatura, ls);
+			map.put(asignatura, set);
 		}
 		System.out.println("Apartado a)\n");
-		List<String> resA = Ejercicio3.apartadoA(map);
+		Set<String> resA = Ejercicio3.apartadoA(map);
 		for(String s: resA) {
 			System.out.println(s);
 		}
 		System.out.println("\nApartado b)\n");
-		List<String> resB = Ejercicio3.apartadoB(map);
+		Set<String> resB = Ejercicio3.apartadoB(map);
 		System.out.println("Las asignaturas que no requieren aprobar otra antes son:");
 		for(String s: resB) {
 			System.out.println("    " + s);
 		}
+		System.out.println("\nApartado c)\n");
+		
+		// Uno de los dos test deben estar comentados
+		
+		// TEST 1
+		
+//		Set<String> set1 = Set.of("Asignatura_01", "Asignatura_02", "Asignatura_03",
+//		                           "Asignatura_04", "Asignatura_05");
+//		Set<String> resC1 = Ejercicio3.apartadoC(map, set1);
+//		System.out.println("· Test 1 – El alumno puede cursar las siguientes asignaturas:");
+//		System.out.println(resC1);
+		
+		// TEST 2
+		
+		Set<String> set2 = Set.of("Asignatura_01", "Asignatura_02", "Asignatura_03",
+                "Asignatura_04", "Asignatura_05", "Asignatura_06", "Asignatura_07",
+                "Asignatura_08", "Asignatura_11");
+		Set<String> resC2 = Ejercicio3.apartadoC(map, set2);
+		System.out.println("\n· Test 2 – El alumno puede cursar las siguientes asignaturas:");
+		System.out.println(resC2);
 	}
 	
 	public static void main(String[] args) {
